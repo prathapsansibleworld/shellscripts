4fb046be21d655c8e498ade6941ca4be1c236f1a
prathapreddy <prathapreddy.s18@gmail.com>
2025-09-19 15:01:25 +0530

    little hearts

---
+++ 02-shell-scripts.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+PERSON1=suresh
+PERSON2=ramesh
+PERSON3=Akhil
+PERSON4=Kathyayani
+
+echo "$PERSON2:: Hi $PERSON1, How are you?"
+echo "$PERSON1:: Hello $PERSON2, I am fine. How are you?"
+echo "$PERSON2:: What are you learning recently?"
+echo "$PERSON1:: I am learning Shell scripting Ramesh"
+echo "$PERSON3:: $PERSON4 bonchesava?"
+echo "$PERSON4:: Orey $PERSON3 edem paata ra"
+echo "$PERSON3:: $PERSON4 phone cheyava"
+echo "$PERSON4:: Orey $PERSON3 phone ledu ra $"
