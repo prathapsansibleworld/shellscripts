4d60a6b674c3b535a02cb870248c49d597e223ae
prathapreddy <prathapreddy.s18@gmail.com>
2025-09-27 09:06:57 +0530

    variable

---
+++ 09-mysqlinstall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+USERID=$(id -u)
+
+if [ $USERID -ne 0]; then
+echo "ERROR:: please use sudo "
+fi
+dnf install mysql -y
+
+if [$? -ne 0]; then
+echo "ERROR: installation is failure"
+else
+echo " Mysql is installed"
+fi
