905bb9199978b28b988a4988c4bb74aa3b9d71cf
prathapreddy <prathapreddy.s18@gmail.com>
2025-09-27 08:31:53 +0530

    variable

--- 07-specialvariables.sh
+++ 07-specialvariables.sh
@@ -2,8 +2,8 @@
 
 echo "All variables:$@"
 echo "All variables are :$*"
-echo"scriptname:$0"
+echo "scriptname:$0"
 cd /var/
-echo"current directory: $PWD"
-echo"home directory user: $HOME"
+echo "current directory: $PWD"
+echo "home directory user: $HOME"
 echo "PID for script:$$"
